86682463733ab8a84fe9f61f916c146f1b8e074a
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2022-01-04 07:41:48 -0500

    create profile view function

---
+++ hood/views.py
@@ -0,0 +1,20 @@
+from typing import Any
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from PIL import Image
+from .forms import *
+from .emails import *
+# Create your views here.
+
+def index(request):
+    Profile=()
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user=request.user
+        profile =Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+    return render(request,'index.html')
