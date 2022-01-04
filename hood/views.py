e717bc777aa116b87dbdf3a8b92fc4057018a97b
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2022-01-04 07:47:49 -0500

    create create_profile view function

--- hood/views.py
+++ hood/views.py
@@ -9,6 +9,7 @@ from .emails import *
 # Create your views here.
 Profile=()
 User=()
+ProfileForm=()
 
 def index(request):
     try:
@@ -30,3 +31,17 @@ def my_profile(request):
 def user_profile(request,username):
     user = User.objects.get(username=username)
     profile =Profile.objects.get(username=user)
+
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user=request.user
+    if request.method=="POST":
+        form =ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.username = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+    else:
+        form = ProfileForm()
+        return render(request,'profile/profile_form.html',{"form":form})
