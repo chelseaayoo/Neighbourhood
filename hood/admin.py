5e9c1ad6d159bc1497f5d804063c024b8db9b608
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2022-01-06 07:34:05 -0500

    add models to admin site

---
+++ hood/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Profile,neighbourhood,BlogPost,Business,Health,healthservices,notifications
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(neighbourhood)
+admin.site.register(BlogPost)
+admin.site.register(Business)
+admin.site.register(Health)
+admin.site.register(healthservices)
+admin.site.register(notifications)
