fbf837f434f2bf02e2bdd2662e3e127c16994efa
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2022-01-06 07:38:39 -0500

    create blogpost class

--- hood/forms.py
+++ hood/forms.py
@@ -5,3 +5,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model=Profile
         exclude=['username']
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model=BlogPost
+        exclude=['username','neighbourhood','profpic']
