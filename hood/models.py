be36d4501b4e60fdb59e6b15e91c566dfb462c6b
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2022-01-06 07:17:59 -0500

    create business class

--- hood/models.py
+++ hood/models.py
@@ -52,3 +52,22 @@ class Comment(models.Model):
     comment = models.CharField(max_length=300)
     username = models.ForeignKey(User,on_delete=models.CASCADE)
     post = models.ForeignKey(BlogPost,on_delete=models.CASCADE)
+
+class Business(models.Model):
+    logo = CloudinaryField('image')
+    description = HTMLField()
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+    address =models.CharField(max_length=100)
+    contact = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    @classmethod
+    def search_business(cls,search_term):
+        businesses = cls.objects.filter(description__icontains=search_term)
+        return businesses
+    
