80309d8cfda84ec01636702ba5f31d2f870c1264
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2022-01-06 07:39:23 -0500

    create commentform class

--- hood/forms.py
+++ hood/forms.py
@@ -10,3 +10,8 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model=BlogPost
         exclude=['username','neighbourhood','profpic']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment
+        exclude=['username','post']
